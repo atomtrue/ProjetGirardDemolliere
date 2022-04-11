7c851def3f7bf0431614d2d3da962a0453a086fb
MaxenceDemo <98153825+MaxenceDemo@users.noreply.github.com>
2022-04-11 13:33:05 +0200

    Update PingNative.java

--- library/src/main/java/com/stealthcopter/networktools/ping/PingNative.java
+++ library/src/main/java/com/stealthcopter/networktools/ping/PingNative.java
@@ -1,7 +1,3 @@
-package com.stealthcopter.networktools.ping;
-
-import com.stealthcopter.networktools.IPTools;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,7 +5,6 @@ import java.net.InetAddress;
 
 public class PingNative {
 
-    // This class is not to be instantiated
     private PingNative() {
     }
 
@@ -32,14 +27,11 @@ public class PingNative {
 
         if (address != null) {
             if (IPTools.isIPv6Address(address)) {
-                // If we detect this is a ipv6 address, change the to the ping6 binary
                 pingCommand = "ping6";
             } else if (!IPTools.isIPv4Address(address)) {
-                // Address doesn't look to be ipv4 or ipv6, but we could be mistaken
 
             }
         } else {
-            // Not sure if getHostAddress ever returns null, but if it does, use the hostname as a fallback
             address = host.getHostName();
         }
 
@@ -67,42 +59,6 @@ public class PingNative {
         proc.destroy();
         return pingResult;
     }
-
-    /**
-     * getPingStats interprets the text result of a Linux activity_ping command
-     *
-     * Set pingError on error and return null
-     *
-     * http://en.wikipedia.org/wiki/Ping
-     *
-     * PING 127.0.0.1 (127.0.0.1) 56(84) bytes of data.
-     * 64 bytes from 127.0.0.1: icmp_seq=1 ttl=64 time=0.251 ms
-     * 64 bytes from 127.0.0.1: icmp_seq=2 ttl=64 time=0.294 ms
-     * 64 bytes from 127.0.0.1: icmp_seq=3 ttl=64 time=0.295 ms
-     * 64 bytes from 127.0.0.1: icmp_seq=4 ttl=64 time=0.300 ms
-     *
-     * --- 127.0.0.1 activity_ping statistics ---
-     * 4 packets transmitted, 4 received, 0% packet loss, time 0ms
-     * rtt min/avg/max/mdev = 0.251/0.285/0.300/0.019 ms
-     *
-     * PING 192.168.0.2 (192.168.0.2) 56(84) bytes of data.
-     *
-     * --- 192.168.0.2 activity_ping statistics ---
-     * 1 packets transmitted, 0 received, 100% packet loss, time 0ms
-     *
-     * # activity_ping 321321.
-     * activity_ping: unknown host 321321.
-     *
-     * 1. Check if output contains 0% packet loss : Branch to success - Get stats
-     * 2. Check if output contains 100% packet loss : Branch to fail - No stats
-     * 3. Check if output contains 25% packet loss : Branch to partial success - Get stats
-     * 4. Check if output contains "unknown host"
-     *
-     * @param pingResult - the current ping result
-     * @param s - result from ping command
-     *
-     * @return The ping result
-     */
     public static PingResult getPingStats(PingResult pingResult, String s) {
         String pingError;
         if (s.contains("0% packet loss")) {
