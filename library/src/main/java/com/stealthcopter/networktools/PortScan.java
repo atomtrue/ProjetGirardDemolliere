032d62eb3e09c17a322c0270e89ca3b06ca176f4
MaxenceDemo <98153825+MaxenceDemo@users.noreply.github.com>
2022-04-11 13:29:27 +0200

    Update PortScan.java

--- library/src/main/java/com/stealthcopter/networktools/PortScan.java
+++ library/src/main/java/com/stealthcopter/networktools/PortScan.java
@@ -1,8 +1,3 @@
-package com.stealthcopter.networktools;
-
-import com.stealthcopter.networktools.portscanning.PortScanTCP;
-import com.stealthcopter.networktools.portscanning.PortScanUDP;
-
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -35,7 +30,6 @@ public class PortScan {
 
     private PortListener portListener;
 
-    // This class is not to be instantiated
     private PortScan() {
     }
 
@@ -45,25 +39,12 @@ public class PortScan {
         void onFinished(ArrayList<Integer> openPorts);
     }
 
-    /**
-     * Set the address to ping
-     *
-     * @param address - Address to be pinged
-     * @return this object to allow chaining
-     * @throws UnknownHostException - if no IP address for the
-     *                              {@code host} could be found, or if a scope_id was specified
-     *                              for a global IPv6 address.
-     */
+
     public static PortScan onAddress(String address) throws UnknownHostException {
         return onAddress(InetAddress.getByName(address));
     }
 
-    /**
-     * Set the address to ping
-     *
-     * @param ia - Address to be pinged
-     * @return this object to allow chaining
-     */
+
     public static PortScan onAddress(InetAddress ia) {
         PortScan portScan = new PortScan();
         portScan.setAddress(ia);
@@ -71,32 +52,13 @@ public class PortScan {
         return portScan;
     }
 
-    /**
-     * Sets the timeout for each port scanned
-     * <p>
-     * If you raise the timeout you may want to consider increasing the thread count {@link #setNoThreads(int)} to compensate.
-     * We can afford to have quite a high thread count as most of the time the thread is just sitting
-     * idle and waiting for the socket to timeout.
-     *
-     * @param timeOutMillis - the timeout for each ping in milliseconds
-     *                      Recommendations:
-     *                      Local host: 20 - 500 ms - can be very fast as request doesn't need to go over network
-     *                      Local network 500 - 2500 ms
-     *                      Remote Scan 2500+ ms
-     * @return this object to allow chaining
-     */
     public PortScan setTimeOutMillis(int timeOutMillis) {
         if (timeOutMillis < 0) throw new IllegalArgumentException("Timeout cannot be less than 0");
         this.timeOutMillis = timeOutMillis;
         return this;
     }
 
-    /**
-     * Scan the ports to scan
-     *
-     * @param port - the port to scan
-     * @return this object to allow chaining
-     */
+
     public PortScan setPort(int port) {
         ports.clear();
         validatePort(port);
@@ -104,12 +66,7 @@ public class PortScan {
         return this;
     }
 
-    /**
-     * Scan the ports to scan
-     *
-     * @param ports - the ports to scan
-     * @return this object to allow chaining
-     */
+
     public PortScan setPorts(ArrayList<Integer> ports) {
 
         // Check all ports are valid
@@ -122,13 +79,7 @@ public class PortScan {
         return this;
     }
 
-    /**
-     * Scan the ports to scan
-     *
-     * @param portString - the ports to scan (comma separated, hyphen denotes a range). For example:
-     *                   "21-23,25,45,53,80"
-     * @return this object to allow chaining
-     */
+
     public PortScan setPorts(String portString) {
 
         ports.clear();
@@ -165,21 +116,12 @@ public class PortScan {
         return this;
     }
 
-    /**
-     * Checks and throws exception if port is not valid
-     *
-     * @param port - the port to validate
-     */
     private void validatePort(int port) {
         if (port < 1) throw new IllegalArgumentException("Start port cannot be less than 1");
         if (port > 65535) throw new IllegalArgumentException("Start cannot be greater than 65535");
     }
 
-    /**
-     * Scan all privileged ports
-     *
-     * @return this object to allow chaining
-     */
+
     public PortScan setPortsPrivileged() {
         ports.clear();
         for (int i = 1; i < 1024; i++) {
@@ -188,11 +130,7 @@ public class PortScan {
         return this;
     }
 
-    /**
-     * Scan all ports
-     *
-     * @return this object to allow chaining
-     */
+
     public PortScan setPortsAll() {
         ports.clear();
         for (int i = 1; i < 65536; i++) {
@@ -206,30 +144,22 @@ public class PortScan {
     }
 
     private void setDefaultThreadsAndTimeouts() {
-        // Try and work out automatically what kind of host we are scanning
-        // local host (this device) / local network / remote
+
         if (IPTools.isIpAddressLocalhost(address)) {
-            // If we are scanning a the localhost set the timeout to be very short so we get faster results
-            // This will be overridden if user calls setTimeoutMillis manually.
+   
             timeOutMillis = TIMEOUT_LOCALHOST;
             noThreads = DEFAULT_THREADS_LOCALHOST;
         } else if (IPTools.isIpAddressLocalNetwork(address)) {
-            // Assume local network (not infallible)
+
             timeOutMillis = TIMEOUT_LOCALNETWORK;
             noThreads = DEFAULT_THREADS_LOCALNETWORK;
         } else {
-            // Assume remote network timeouts
+
             timeOutMillis = TIMEOUT_REMOTE;
             noThreads = DEFAULT_THREADS_REMOTE;
         }
     }
 
-    /**
-     * @param noThreads set the number of threads to work with, note we default to a large number
-     *                  as these requests are network heavy not cpu heavy.
-     * @return self
-     * @throws IllegalArgumentException - if no threads is less than 1
-     */
     public PortScan setNoThreads(int noThreads) throws IllegalArgumentException {
         if (noThreads < 1) throw new IllegalArgumentException("Cannot have less than 1 thread");
         this.noThreads = noThreads;
@@ -237,13 +167,7 @@ public class PortScan {
     }
 
 
-    /**
-     * Set scan method, either TCP or UDP
-     *
-     * @param method - the transport method to use to scan, either PortScan.METHOD_UDP or PortScan.METHOD_TCP
-     * @return this object to allow chaining
-     * @throws IllegalArgumentException - if invalid method
-     */
+
     private PortScan setMethod(int method) {
         switch (method) {
             case METHOD_UDP:
@@ -256,39 +180,22 @@ public class PortScan {
         return this;
     }
 
-    /**
-     * Set scan method to UDP
-     *
-     * @return this object to allow chaining
-     */
+
     public PortScan setMethodUDP() {
         setMethod(METHOD_UDP);
         return this;
     }
 
-    /**
-     * Set scan method to TCP
-     *
-     * @return this object to allow chaining
-     */
     public PortScan setMethodTCP() {
         setMethod(METHOD_TCP);
         return this;
     }
 
 
-    /**
-     * Cancel a running ping
-     */
     public void cancel() {
         this.cancelled = true;
     }
 
-    /**
-     * Perform a synchronous (blocking) port scan and return a list of open ports
-     *
-     * @return - ping result
-     */
     public ArrayList<Integer> doScan() {
 
         cancelled = false;
@@ -301,10 +208,7 @@ public class PortScan {
             executor.execute(worker);
         }
 
-        // This will make the executor accept no new threads
-        // and finish all existing threads in the queue
         executor.shutdown();
-        // Wait until all threads are finish
         try {
             executor.awaitTermination(1, TimeUnit.HOURS);
         } catch (InterruptedException e) {
@@ -316,12 +220,6 @@ public class PortScan {
         return openPortsFound;
     }
 
-    /**
-     * Perform an asynchronous (non-blocking) port scan
-     *
-     * @param portListener - the listener to fire portscan results to.
-     * @return - this object so we can cancel the scan if needed
-     */
     public PortScan doScan(final PortListener portListener) {
 
         this.portListener = portListener;
@@ -339,10 +237,7 @@ public class PortScan {
                     executor.execute(worker);
                 }
 
-                // This will make the executor accept no new threads
-                // and finish all existing threads in the queue
                 executor.shutdown();
-                // Wait until all threads are finish
                 try {
                     executor.awaitTermination(1, TimeUnit.HOURS);
                 } catch (InterruptedException e) {
