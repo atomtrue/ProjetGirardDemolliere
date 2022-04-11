2704aadd585844f58ab3baaaa6ade6372c295efe
MaxenceDemo <98153825+MaxenceDemo@users.noreply.github.com>
2022-04-11 13:28:03 +0200

    Update MACTools.java

--- library/src/main/java/com/stealthcopter/networktools/MACTools.java
+++ library/src/main/java/com/stealthcopter/networktools/MACTools.java
@@ -1,4 +1,3 @@
-package com.stealthcopter.networktools;
 
 import java.util.regex.Pattern;
 
@@ -8,31 +7,14 @@ public class MACTools {
             Pattern.compile(
                     "^([0-9A-Fa-f]{2}[\\.:-]){5}([0-9A-Fa-f]{2})$");
 
-    // This class is not to be instantiated
     private MACTools() {
     }
 
 
-    /**
-     * Validates a provided MAC address
-     *
-     * @param macAddress - the MAC address to check
-     * @return - true if it is valid MAC address in IEEE802 format (either hyphen or colon separated)
-     * eg: "01:23:45:67:89:AB" or "01-23-45-67-89-AB"
-     */
     public static boolean isValidMACAddress(final String macAddress) {
         return macAddress != null && PATTERN_MAC.matcher(macAddress).matches();
     }
 
-
-    /**
-     * Convert a MAC string to bytes
-     *
-     * @param macStr - MAC string in IEEE802 format (either hyphen or colon separated)
-     *               eg: "01:23:45:67:89:AB" or "01-23-45-67-89-AB"
-     * @return - MAC formatted in bytes
-     * @throws IllegalArgumentException - if mac address is invalid
-     */
     public static byte[] getMacBytes(String macStr) throws IllegalArgumentException {
 
         if (macStr == null) throw new IllegalArgumentException("Mac Address cannot be null");
